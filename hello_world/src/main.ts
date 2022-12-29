83d9c6298ca1c1c48c49160cd95b2a9a695886f8
shrinivas1080 <shrinivas264.ht@gmail.com>
2022-12-29 12:33:31 +0530

    first commit

---
+++ hello_world/src/main.ts
@@ -0,0 +1,8 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+//import { AppModule } from './app/test/test.module';
+
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
